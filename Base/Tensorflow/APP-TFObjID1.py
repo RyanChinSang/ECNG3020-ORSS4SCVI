6bdf9d87db53b844b606ba22b93c2582c3da050b
RyanChinSang <ryancs1995@gmail.com>
2017-11-01 14:59:22 -0400

    Added ColorDetection. Started work for dev0.1a - base code added.

---
+++ Base/Tensorflow/APP-TFObjID1.py
@@ -0,0 +1,109 @@
+import numpy as np
+import os
+import tensorflow as tf
+import cv2
+from threading import Thread
+from Base.Tensorflow.utils import visualization_utils as vis_util
+from Base.Tensorflow.utils import label_map_util
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0, width=480, height=360):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        (self.grabbed, self.frame) = self.stream.read()
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=(), daemon=True).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stream.release()
+        self.stopped = True
+
+
+# Global constant names
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
+
+
+# Load label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map,
+                                                            max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+detection_graph = tf.Graph()
+cv2.setUseOptimized(True)
+cap = WebcamVideoStream().start()
+
+with detection_graph.as_default():
+    # Load a frozen Tensorflow model into memory.
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+    # Detection
+    with tf.Session(graph=detection_graph) as sess:
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        while 1:
+            image_np = cap.read()
+            s = cv2.getTickCount()
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=2)
+
+            f = cv2.getTickCount()
+            cv2.putText(img=image_np,
+                        text='fps: {:.2f}'.format(cv2.getTickFrequency()/(f-s)),
+                        org=(0, 15),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=0.5,
+                        color=(255, 255, 255),
+                        thickness=1,
+                        lineType=cv2.LINE_AA,
+                        bottomLeftOrigin=False)
+            cv2.imshow('image', image_np)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cap.stop()
+                cv2.destroyAllWindows()
+                break
